c3f5d75020ac4e929ffd0a332c9bbd8f86ab91f7
rabby159 <ablazerabbi159@gmail.com>
2025-01-22 00:47:50 +0600

    if condition

---
+++ if.js
@@ -0,0 +1,15 @@
+/**
+ * if(condition){
+ *      implementation code if condition is a match.
+ * } else {
+ *      implementation code if condition is a not-match.
+ * }
+ */
+
+const myHeadphonePrice = 1899;
+
+if(myHeadphonePrice > 4000){
+    console.log("Ami Borolox Pro");
+} else{
+    console.log("Ami always gorib");
+}
