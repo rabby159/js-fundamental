721c60cf29fb011d43e2a73d41ccb7830826ee50
rabby159 <ablazerabbi159@gmail.com>
2025-01-21 23:09:02 +0600

    comparison fundamental

---
+++ comparison.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Comparison
+ * 1. Greater: >
+ * 2. Less: <
+ * 3. Equal: ==
+ * 4. Greater then or equal: >=
+ * 5. Less then or equal: <=
+ * 6. not equal: !=
+ * 7. and: $$
+ * 8. or: ||
+ * 9. == ; Check only equal
+ * 10. === ; check equal an data type also
+ *
+ */
+
+// console.log(15 > 10);
+// console.log(10 < 12);
+// console.log(10 == 10);
+// console.log(25 >= 25);
+// console.log(13 <= 13);
+// console.log(10 != 6);
+// console.log(10 == "10");
+// console.log(10 === "10");
