0f7a79795b490ba9777cc3225a98ae20dd3b0c2c
rabby159 <ablazerabbi159@gmail.com>
2025-01-22 01:06:31 +0600

    if and & or

--- if.js
+++ if.js
@@ -8,8 +8,26 @@
 
 const myHeadphonePrice = 1899;
 
-if(myHeadphonePrice > 4000){
-    console.log("Ami Borolox Pro");
-} else{
-    console.log("Ami always gorib");
+if (myHeadphonePrice > 4000) {
+  //   console.log("Ami Borolox Pro");
+} else {
+  //   console.log("Ami always gorib");
+}
+// -------------------------------------------------------------------------------
+const mySalary = 25000;
+const IsGraduation = false;
+const age = 25;
+
+// --------------------------AND-----------------------------------------------------
+if (mySalary > 20000 && age > 21 && IsGraduation === true) {
+  // console.log("bibho joggo");
+} else {
+  // console.log("tr dara hbe nah");
+}
+
+// ---------------------------OR----------------------------------------------------
+if (mySalary > 30000 || age > 21 || IsGraduation === true) {
+  console.log("bibho joggo");
+} else {
+  console.log("tr dara hbe nah");
 }
