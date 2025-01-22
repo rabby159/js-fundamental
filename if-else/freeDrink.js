b0b96b23759c318910c8b6bb8797db26ea48d227
rabby159 <ablazerabbi159@gmail.com>
2025-01-23 01:58:07 +0600

    arr

--- if-else/freeDrink.js
+++ if-else/freeDrink.js
@@ -8,8 +8,8 @@ Free Drinks
 const burgerPrice = 510;
 const coke = 30;
 
-if(burgerPrice > 500){
-    console.log("You get Free Coke");
-} else{
-    console.log(coke);
+if (burgerPrice > 500) {
+  console.log("You get Free Coke");
+} else {
+  console.log(coke);
 }
