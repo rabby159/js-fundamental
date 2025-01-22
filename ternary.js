0468700d194c342f2a6b0f6be6a6d1dd322684ee
rabby159 <ablazerabbi159@gmail.com>
2025-01-22 23:33:44 +0600

    ternarry version

--- ternary.js
+++ ternary.js
@@ -11,15 +11,23 @@ also, write it using ternary operator.
 
  */
 
-const num1 = 10;
+const num1 = 50;
 const num2 = 20;
 
 let result;
 
-if(num1 > num2){
-    console.log(result = num1 + num1);
-}
-else{
-    console.log(result = num1 + num2);
+if (num1 > num2) {
+  //   console.log((result = num1 + num1));
+} else {
+  //   console.log((result = num1 + num2));
 }
 
+//------------------------------------------------------------
+
+const num1GreaterThenNum2 = num1 < num2;
+
+if (!num1GreaterThenNum2) {
+  console.log((result = num1 + num1));
+} else {
+  console.log((result = num1 + num2));
+}
