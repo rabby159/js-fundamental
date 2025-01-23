852a2686e4fb10f53b33a5f8e9f8fb0365eda722
rabby159 <ablazerabbi159@gmail.com>
2025-01-23 13:18:03 +0600

    array pop

--- array/push-pop.js
+++ array/push-pop.js
@@ -1,8 +1,11 @@
 const id = [2, 1, 3, 9, 0];
 
-console.log(id);
-id.push(2); //add new value in array
+// console.log(id);
+id.push(2); //add last index new value in array
 id.push(1);
 id.push(2);
 id.push(4);
+console.log(id);
+
+id.pop(id); //remove last index value from array
 console.log(id);
