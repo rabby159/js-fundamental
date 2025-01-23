16962064b63b03461556cd7aeab0344b322020b8
rabby159 <ablazerabbi159@gmail.com>
2025-01-23 12:45:41 +0600

    arr update value

--- array/arr.js
+++ array/arr.js
@@ -1,6 +1,10 @@
 
 
 const number = [1, 2, 5, 6, 8];
+console.log(number);
 console.log(number.length);  //length
 
 console.log(number[1]); //index(always start with 0)
+
+number[0] = 2124; //update index value
+console.log(number);
