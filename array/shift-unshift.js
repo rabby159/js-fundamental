754b6aea28edbee388fefa9a6271ec4d0fa3b82f
rabby159 <ablazerabbi159@gmail.com>
2025-01-23 13:20:47 +0600

    array shift-unshift

---
+++ array/shift-unshift.js
@@ -0,0 +1,8 @@
+const id = [2, 1, 3, 9, 0];
+console.log(id);
+
+id.shift(id); //remove fast index value from array
+console.log(id);
+
+id.unshift(100); //add index index new value in array
+console.log(id);
