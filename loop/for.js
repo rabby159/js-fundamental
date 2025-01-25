44f706e374cc7722cbe154b7cda69206e2e1a081
rabby159 <ablazerabbi159@gmail.com>
2025-01-25 13:23:04 +0600

    even and odd for loop

--- loop/for.js
+++ loop/for.js
@@ -1,6 +1,6 @@
 /**
  * 
- * for(first statement; second statement; third statement){
+ * for(expression1; expression2; expression3){
  *      loop statement;
  * }
  * 
@@ -8,4 +8,14 @@
 
 for(let num = 0; num < 10; num++){
     console.log(num);
+}
+
+//even
+for(let i = 0; i < 20; i=i+2){
+    console.log(i);
+}
+
+//odd
+for(let i = 1; i < 20; i=i+2){
+    console.log(i);
 }
