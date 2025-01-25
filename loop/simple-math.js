93246e41f4747d50eb00d4ba50e03b23bdb85ba0
rabby159 <ablazerabbi159@gmail.com>
2025-01-25 13:53:07 +0600

    simple 2 & 3

--- loop/simple-math.js
+++ loop/simple-math.js
@@ -5,3 +5,17 @@ for(let i = 0; i <= 30; i++){
         console.log(i);
     }
 }
+
+//give me the list of 1 to 30 divisible of 3 or 5
+for(let i =0; i <= 30; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+        console.log(i);
+    }
+}
+
+//give me the list of 1 to 30 divisible of 3 and 5
+for(let i =0; i <= 30; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i);
+    }
+}
