0afab0f6deaa501c717a8b0c97a2b59f2727d529
rabby159 <ablazerabbi159@gmail.com>
2025-01-25 22:18:56 +0600

    string

--- loop/simple-math.js
+++ loop/simple-math.js
@@ -1,21 +1,21 @@
 //give me the list of 1 to 30 divisible of 5
 
-for(let i = 0; i <= 30; i++){
-    if(i % 5 === 0){
-        console.log(i);
-    }
+for (let i = 0; i <= 30; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
 }
 
 //give me the list of 1 to 30 divisible of 3 or 5
-for(let i =0; i <= 30; i++){
-    if(i % 3 === 0 || i % 5 === 0){
-        console.log(i);
-    }
+for (let i = 0; i <= 30; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    console.log(i);
+  }
 }
 
 //give me the list of 1 to 30 divisible of 3 and 5
-for(let i =0; i <= 30; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log(i);
-    }
+for (let i = 0; i <= 30; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i);
+  }
 }
---
+++ string/string.js
@@ -0,0 +1,6 @@
+const country = "Bangladesh";
+const district = 'Dhaka';
+const thana = `Khilgaon`;
+const area = new String("Meradia");
+
+console.log(typeof area);
