611175c0fd6c8899910bdac21008964c7713c0c0
rabby159 <ablazerabbi159@gmail.com>
2025-01-25 23:21:36 +0600

    object

---
+++ object/object.js
@@ -0,0 +1,10 @@
+const myInfo ={
+    name : 'rabby',
+    age : 25,
+    friends: ["selim", 'jubayer', 'roni', 'ripon'],
+    university: {
+        name : 'GUB',
+        myId : 213902124,
+        dept : "CSE"
+    }
+}
---
+++ string/reverse.js
@@ -0,0 +1,10 @@
+
+const intro = "I am junior web developer";
+
+let reverse = '';
+
+for(const character of intro){
+    reverse = character + reverse;
+};
+
+console.log(reverse);
