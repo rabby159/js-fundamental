fa3bcc79d96559acf6ba4151c6dc142d5cbfb080
rabby159 <ablazerabbi159@gmail.com>
2025-01-27 01:21:32 +0600

    conditonal return odd

--- function/conditional_return.js
+++ function/conditional_return.js
@@ -1,14 +1,25 @@
-
-function isEven(num1){
-    if(num1 % 2 === 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-};
+function isEven(num1) {
+  if (num1 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 const resultEven = isEven(5);
 const resultEven2 = isEven(10);
 
-console.log(resultEven, resultEven2);
+// console.log(resultEven, resultEven2);
+
+function isOdd(num1) {
+  if (num1 % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const resultOdd = isOdd(5);
+const resultOdd2 = isOdd(10);
+
+console.log(resultOdd, resultOdd2);
