65b1b590123333feceef242eb87be564313c192b
rabby159 <ablazerabbi159@gmail.com>
2025-01-26 22:36:51 +0600

    looping use for

--- array/looping.js
+++ array/looping.js
@@ -1,7 +1,12 @@
-const drink = ['water', 'mojo', 'cola', 'jola'];
+const drink = ['water', 'mojo', 'cola', 'jola', 'hola'];
 
 
 for(let khabo of drink){
-    console.log(khabo);
+    // console.log(khabo);
 };
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
+    console.log(drink[i]);
+}
 
