84d9741746561fa0f1c306a312785d3d6fa31096
rabby159 <ablazerabbi159@gmail.com>
2025-01-27 00:54:30 +0600

    argument

---
+++ function/argument.js
@@ -0,0 +1,8 @@
+function checkPara(num1, num2) {
+    sum = num1 * num2;
+    return sum;
+  }
+  
+  checkPara(2, 6); //(2, 6) ---> argument
+  console.log(sum);
+  
--- function/parameter.js
+++ function/parameter.js
@@ -1,9 +1,7 @@
-
-
-function checkPara(a, b){
-    sum = a + b;
-    return sum;
-};
+function checkPara(a, b) { //(a, b) -- parameter
+  sum = a + b;
+  return sum;
+}
 
 checkPara(2, 6);
 console.log(sum);
