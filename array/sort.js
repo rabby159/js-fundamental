011e3c9d4c6462f832d7e6edec1b769bf7dc6652
rabby159 <ablazerabbi159@gmail.com>
2025-01-26 23:02:51 +0600

    sort

--- array/sort.js
+++ array/sort.js
@@ -3,4 +3,8 @@ const character = ["d", "c", "b", "a", "g"];
 const sort = character.sort()
 console.log(sort);
 
-
+/**
+ * Possible to two sort
+ * 1. ascending --->[1, 2, 3, 4, 5, 6, 7]
+ * 2. descending --->[7, 6, 5, 4, 3, 2, 1]
+ */
